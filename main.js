4b6249d0df70cbdb530191d2ede08a83399a3939
Francisco Santin <franciscosantin2002@gmail.com>
2023-09-06 18:55:32 -0300

    algunos errores arreglados

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import { PuntoR3, PuntoR2, Espacio, Observador, Grafico, } from './src/clases/clases';
 import { convertirAUnitario, prodConEscalar, prodVectorial, sumaVec, moduloVector, vectorOpuesto } from './src/operaciones/operaciones_vectores'
 
+const grafico = new Grafico();
+let espacio = new Espacio(),
+    obs = new Observador( new PuntoR3( 0, 0, 20 ), new PuntoR3( 0, 0, -1 ), new PuntoR3( 1, 0, 0 ), new PuntoR3( 0, 1, 0 ) );
+
+espacio.agregarPunto( new PuntoR3( 0, 0, 0 ) );
+
+grafico.render( espacio, obs );
--- src/clases/clases.js
+++ src/clases/clases.js
@@ -1,5 +1,9 @@
 import { prodConEscalar, sumaVec, convertirAUnitario, prodVectorial } from '../operaciones/operaciones_vectores';
 
+const grafico = document.querySelector('.grafico');
+const posGraficoX = ( x ) => grafico.offsetWidth / 2. + x;
+const posGraficoY = ( y ) => grafico.offsetHeight / 2. - y;
+
 export class PuntoR3 {
     x;
     y;
@@ -15,16 +19,33 @@ export class Espacio {
     puntos = [];
     triangulos = [];
     lineas = [];
+    
+    agregarPunto( ptoR3 ) {
+        this.puntos.push( ptoR3 );
+    }
+
+    agregarTriangulo( triangulo ) {
+        this.triangulos.push(triangulo.puntos);
+    }
+
+    agregarLinea( linea ) {
+        this.lineas.push( linea.puntos );
+    }
+
 }
 
 export class Observador {
     pos;
     dir;
+    u;
+    v;
     static angGiro = 0.08819;
     static pasoAvance = 1;
-    constructor(pos, dir) {
+    constructor(pos, dir, u, v) {
         this.pos = pos;
         this.dir = dir;
+        this.u = u; 
+        this.v = v;
     }
 
     girarArriba( ) {
@@ -72,36 +93,31 @@ export class Observador {
 
 }
 
-export class Grafico {
-    divHTML;
-    constructor() {
-      this.divHTML = document.querySelector('.grafico');
-    }
-}
-
 export class PuntoR2 {
     x;
     y;
+    ptoR3;
     visible = false;
     divHTML;
-    constructor( ptoR3 ) {
+    constructor( ptoR3, obs ) {
+        this.ptoR3 = ptoR3;
         const d = -1. * ( obs.dir.x * ptoR3.x + obs.dir.y * ptoR3.y + obs.dir.z * ptoR3.z ),
               lambda = ( -d - obs.dir.x * obs.pos.x - obs.dir.y * obs.pos.y - obs.dir.z * obs.pos.z ) / ( obs.dir.x ** 2. + obs.dir.y ** 2. + obs.dir.z ** 2. );
         const origenR3 = new PuntoR3( obs.pos.x + lambda * obs.dir.x, obs.pos.y + lambda * obs.dir.y, obs.pos.z + lambda * obs.dir.z ),
               dist = Math.sqrt( (origenR3.x - obs.pos.x) ** 2. + (origenR3.y - obs.pos.y) ** 2. + (origenR3.z - obs.pos.z) ** 2. ); 
-        let aux = ( u.y * v.x - u.x * v.y );
+        let aux = ( obs.u.y * obs.v.x - obs.u.x * obs.v.y );
         if( aux != 0 ) {
-            this.y = ( u.y * ( ptoR3.x - origenR3.x ) - u.x * ( ptoR3.y - origenR3.y ) ) / aux;
-            this.x = ( v.y * ( ptoR3.x - origenR3.x ) - v.x * ( ptoR3.y - origenR3.y ) ) / (-aux);
+            this.y = ( obs.u.y * ( ptoR3.x - origenR3.x ) - obs.u.x * ( ptoR3.y - origenR3.y ) ) / aux;
+            this.x = ( obs.v.y * ( ptoR3.x - origenR3.x ) - obs.v.x * ( ptoR3.y - origenR3.y ) ) / (-aux);
         } else {
-            aux = ( u.z * v.x - u.x * v.z );
+            aux = ( obs.u.z * obs.v.x - obs.u.x * obs.v.z );
             if( aux != 0 ) {
-                this.y = ( u.z * ( ptoR3.x - origenR3.x ) - u.x * ( ptoR3.z - origenR3.z ) ) / aux;
-                this.x = ( v.z * ( ptoR3.x - origenR3.x ) - v.x * ( ptoR3.z - origenR3.z ) ) / (-aux);
+                this.y = ( obs.u.z * ( ptoR3.x - origenR3.x ) - obs.u.x * ( ptoR3.z - origenR3.z ) ) / aux;
+                this.x = ( obs.v.z * ( ptoR3.x - origenR3.x ) - obs.v.x * ( ptoR3.z - origenR3.z ) ) / (-aux);
             } else {
-                aux = ( u.z * v.y - u.y * v.z );
-                this.y = ( u.z * ( ptoR3.y - origenR3.y ) - u.y * ( ptoR3.z - origenR3.z ) ) / aux;
-                this.x = ( v.z * ( ptoR3.y - origenR3.y ) - v.y * ( ptoR3.z - origenR3.z ) ) / (-aux);
+                aux = ( obs.u.z * obs.v.y - obs.u.y * obs.v.z );
+                this.y = ( obs.u.z * ( ptoR3.y - origenR3.y ) - obs.u.y * ( ptoR3.z - origenR3.z ) ) / aux;
+                this.x = ( obs.v.z * ( ptoR3.y - origenR3.y ) - obs.v.y * ( ptoR3.z - origenR3.z ) ) / (-aux);
             }
         }
         this.x = (this.x / dist) * grafico.offsetHeight;
@@ -119,8 +135,10 @@ export class PuntoR2 {
 export class LineaR2 {
     pto1;
     pto2;
+    puntos = [];
     divHTML;
     constructor(pto1, pto2) {
+        this.puntos.push( pto1.ptoR3, pto2.ptoR3 );
         const aux1 = posGraficoY( pto1.y ),
               aux2 = posGraficoX( pto1.x ),
               aux3 = posGraficoY( pto2.y ),
@@ -142,8 +160,10 @@ export class Triangulo {
     ver1;
     ver2;
     ver3;
+    puntos = []
     divHTML;
     constructor( ver1, ver2, ver3 ) {
+        this.puntos.push( ver1.ptoR3, ver2.ptoR3, ver3.ptoR3 );
         const deltaX = ver2.x - ver1.x,
               deltaY = ver2.y - ver1.y,
               deltaX2 = ver3.x - ver1.x,
@@ -169,5 +189,31 @@ export class Triangulo {
         this.divHTML.style.borderLeft = `${ aux2 }px solid transparent`;
         this.divHTML.style.borderRight = `${ width - aux2 }px solid transparent`;
         this.divHTML.style.borderTop = `${ aux1 }px solid blue`;
+    }
+}
+
+export class Grafico {
+    divHTML;
+    constructor() {
+      this.divHTML = document.querySelector('.grafico');
+    }
+
+    render( espacio, obs ) {
+        this.divHTML.replaceChildren('');
+        espacio.puntos.forEach( elem => {
+            const ptoR2 = new PuntoR2( elem, obs )
+            this.divHTML.append( ptoR2.divHTML );
+        });
+    
+        espacio.triangulos.forEach( elem => {
+            const triangulo = new Triangulo( new PuntoR2(elem.puntos[0], obs), new PuntoR2(elem.puntos[1], obs), new PuntoR2(elem.puntos[2], obs) );
+            this.divHTML.append( triangulo.divHTML );
+        });
+    
+        espacio.lineas.forEach( elem => {
+            const linea = new LineaR2( new PuntoR2( elem.puntos[0], obs ), new PuntoR2( elem.puntos[1] ), obs );
+            this.divHTML.append( linea.divHTML );
+        });
+    
     }
 }
